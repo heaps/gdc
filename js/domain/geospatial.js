665bbe6d08631e1569484adc2e8f3119ec345b18
jrw0mitedu <jrwtango@gmail.com>
2017-11-22 11:14:09 -0500

    geospatial

---
+++ js/domain/geospatial.js
@@ -0,0 +1,27 @@
+// the domain banner 
+// ----------------------------------
+
+var banner = {
+    title       : 'GeoSpatial',
+    description : 'Lidar and photogrametry provide detailed data to detect change. We have developed cloud computing services for machine learning to analyze large datasets',
+    image       : 'img/geospatial/powerlinelidar.jpg',
+    linkIsVideo : true,    
+    link        : 'https://www.youtube.com/embed/8ICOM6_q7m4'
+};
+
+
+// the domain videos
+// ----------------------------------
+
+var boxes = [];
+boxes.push({
+    title       : 'Social Media',
+    subtitle    : 'Social Network Opportunities',
+    description : 'Disrupting social media rules of engagement',
+    url         : 'https://www.youtube.com/embed/4Q-WYFm2dl0',
+    video       : true,
+    thumbnail   : 'images/social_media_play.jpg'
+});
+
+
+
